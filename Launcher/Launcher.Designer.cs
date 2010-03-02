5187a4ee8796f104c5cfdb06428bbd9f625e275b
Scott Mundorff <scott.mundorff@gmail.com>
2010-03-01 19:18:48 -0500

    Add application launcher project

--- Launcher/Launcher.Designer.cs
+++ Launcher/Launcher.Designer.cs
@@ -28,21 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Launcher
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Launcher";
-            this.Text = "Launcher";
-            this.Load += new System.EventHandler(this.Launcher_Load);
-            this.ResumeLayout(false);
+			this.label1 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.fileNameColumn = new System.Windows.Forms.ColumnHeader();
+			this.fileTypeColumn = new System.Windows.Forms.ColumnHeader();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(122, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Select the file to Launch";
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileNameColumn,
+            this.fileTypeColumn});
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.listView1.Location = new System.Drawing.Point(0, 30);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(364, 141);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+			// 
+			// fileNameColumn
+			// 
+			this.fileNameColumn.Text = "Filename";
+			this.fileNameColumn.Width = 288;
+			// 
+			// fileTypeColumn
+			// 
+			this.fileTypeColumn.Text = "Type";
+			this.fileTypeColumn.Width = 72;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(272, 8);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(87, 17);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Always show";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// Launcher
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(364, 171);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label1);
+			this.Name = "Launcher";
+			this.Text = "Launcher";
+			this.Load += new System.EventHandler(this.Launcher_Load);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Launcher_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader fileNameColumn;
+		private System.Windows.Forms.ColumnHeader fileTypeColumn;
+		private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -6,11 +6,19 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+using Library;
 
 namespace Launcher
 {
     public partial class Launcher : Form
     {
+		string settingsFile = "launcher.data";
+		string fileName;
+		string fileArgs;
+
         public Launcher()
         {
             InitializeComponent();
@@ -18,7 +26,39 @@ namespace Launcher
 
         private void Launcher_Load(object sender, EventArgs e)
         {
-
+			if (File.Exists(settingsFile))
+			{
+				Process pr = new Process();
+				pr.StartInfo = (ProcessStartInfo)Serial.load(settingsFile);
+				pr.Start();
+				this.Close();
+			}
+			foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.exe", SearchOption.AllDirectories))
+			{
+				if (Path.GetFileName(file) != "Launcher.exe")
+				{
+					ListViewItem item = new ListViewItem(Path.GetFileName(file));
+					item.Tag = file;
+					FileInfo info = new FileInfo(file);
+					item.SubItems.Add(info.Extension);
+					listView1.Items.Add(item);
+				}
+			}
         }
+
+		private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process pr = new Process();
+			pr.StartInfo = new ProcessStartInfo((sender as ListView).SelectedItems[0].Tag.ToString());
+			pr.Start();
+			this.Close();
+			if (checkBox1.Checked == false)
+				Serial.Save(new ProcessStartInfo(fileName, fileArgs), settingsFile);
+		}
+
+		private void Launcher_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			
+		}
     }
 }
